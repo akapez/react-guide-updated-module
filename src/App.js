537628422fa3e90daddbcafcccce39bebacb1029
avishka964 <avishkakapuruge@gmail.com>
2021-11-16 13:08:30 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Header from './components/Header';
+import Welcome from './screens/Welcome';
+import Products from './screens/Products';
+import ProductDetails from './screens/ProductDetails';
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Header />
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/welcome' />
+        </Route>
+        <Route path='/welcome'>
+          <Welcome />
+        </Route>
+        <Route path='/products' exact>
+          <Products />
+        </Route>
+        <Route path='/products/:productId'>
+          <ProductDetails />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/screens/ProductDetails.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {useParams} from 'react-router-dom';
+
+const ProductDetails = () => {
+
+    const params = useParams();
+
+
+
+    return (
+        <section>
+            <h1>Product Details</h1>
+            <p>{params.productId}</p>
+        </section>
+    )
+}
+
+export default ProductDetails
+
